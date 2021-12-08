ede9e09e1e5abc7d3fa649ca716f6cbb2592d9d6
Beck LaBash <becklabash@gmail.com>
2021-12-08 13:46:57 -0500

    2

--- 2/day2.rkt
+++ 2/day2.rkt
@@ -10,13 +10,13 @@
      (define (process-commands x y z commands)
        (if (empty? commands) (make-pos x y z)
            (case (car (first commands))
-         [("forward") (process-commands (+ x (string->number (first (cdr (first commands)))))
-                                        (+ y (* z (string->number (first (cdr (first commands))))))
-                                        z
-                                        (rest commands))]
-         [else (process-commands x y
-                                 ((if (string=? (car (first commands)) "down") + -)
-                                  z (string->number (first (cdr (first commands)))))
+             [("forward") (process-commands (+ x (string->number (first (cdr (first commands)))))
+                                            (+ y (* z (string->number (first (cdr (first commands))))))
+                                            z
+                                            (rest commands))]
+             [else (process-commands x y
+                                     ((if (string=? (car (first commands)) "down") + -)
+                                      z (string->number (first (cdr (first commands)))))
                                      (rest commands))])))
      ]
   (process-commands 0 0 0 commands)))
@@ -28,5 +28,5 @@
 (* (pos-x FINAL-POS) (pos-z FINAL-POS))
 
 ;; Part 2
-(* (pos-x FINAL-POS) (pos-y FINAL-POS))
+(* (pos-x FINAL-POS) (pos-y FINAL-POS-2))
 
